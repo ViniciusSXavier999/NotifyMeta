ef81c8cf3201e9628017ac2c1200ce2d5bb3118e
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2022-09-07 03:55:32 -0300

    Project created

---
+++ backend/src/test/java/com/devsuperior/notifymeta/NotifymetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.notifymeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifymetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

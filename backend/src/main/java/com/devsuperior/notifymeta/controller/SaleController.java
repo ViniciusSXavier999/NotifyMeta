2ca408ea065a056a7b6051350c89eff86e0892b4
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2022-09-09 14:39:16 -0300

    Date select

--- backend/src/main/java/com/devsuperior/notifymeta/controller/SaleController.java
+++ backend/src/main/java/com/devsuperior/notifymeta/controller/SaleController.java
@@ -1,10 +1,11 @@
 package com.devsuperior.notifymeta.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.notifymeta.entities.Sale;
@@ -22,8 +23,11 @@ public class SaleController {
 	
 	@GetMapping
 	// Método para disponibilizar as vendas para o front end
-	public List<Sale> findSales() {
-		return service.findSales();
+	public Page<Sale> findSales(
+			@RequestParam (value = "minDate", defaultValue = "")String minDate,
+			@RequestParam (value = "maxDate", defaultValue = "")String maxDate,
+			Pageable pageable) {
+		return service.findSales(minDate, maxDate, pageable);
 		
 	}
 }
--- backend/src/main/java/com/devsuperior/notifymeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/notifymeta/repositories/SaleRepository.java
@@ -1,11 +1,21 @@
 package com.devsuperior.notifymeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.notifymeta.entities.Sale;
 
 // Componente do sistema responsavel por acessar o banco de dados, consulta, salvar, deletar etc.
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+	
+	// Comando na linguagem JPQL, semelhante a linguagem sql.
+	
+	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 
 }

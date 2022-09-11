1834ab80c927b47ba4b92fbdd5ed783c5dcb3e6b
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2022-09-11 03:09:29 -0300

    Twilio SMS

--- backend/src/main/java/com/devsuperior/notifymeta/controller/SaleController.java
+++ backend/src/main/java/com/devsuperior/notifymeta/controller/SaleController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.notifymeta.entities.Sale;
 import com.devsuperior.notifymeta.services.SaleService;
+import com.devsuperior.notifymeta.services.SmsService;
 
 // Para definir que a classe vai ser uma classe controladora
 
@@ -20,6 +22,9 @@ public class SaleController {
 	@Autowired
 	private SaleService service;
 	
+	@Autowired
+	private SmsService smsService;
+	
 	
 	@GetMapping
 	// Método para disponibilizar as vendas para o front end
@@ -29,5 +34,10 @@ public class SaleController {
 			Pageable pageable) {
 		return service.findSales(minDate, maxDate, pageable);
 		
+	}
+	
+	@GetMapping("{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		smsService.sendSms(id);
 	}
 }

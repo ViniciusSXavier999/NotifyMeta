2c812714daa6c43c1f37a12a618db0458c27252a
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2022-09-11 20:58:45 -0300

    Sale listing

---
+++ frontend/src/utils/request.ts
@@ -0,0 +1,3 @@
+
+// Exportando uma constante, aqui eu estou falando para pegar o valo da variavel de ambiente. 
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";
